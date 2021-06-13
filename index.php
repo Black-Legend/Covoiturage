de92b1f628f760060b05f360d2f8b5780c137bde
Black Legend <agbogawoluck@gmail.com>
2021-06-13 08:50:19 +0000

    Mise en ordre des sections

--- index.php
+++ index.php
@@ -264,33 +264,6 @@
         </div><!-- /.container -->
     </section><!-- /.End of Counter up area -->
 
-    <!-- SYLVAIN pour Avantages  -->
-    <!-- interact user -->
-    <section class="pt-120 pb-120">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-5 col-sm-5">
-                    <!-- user ineract text -->
-                    <div class="user-interact-inner">
-                        <h2>Le covoiturage, c'est économique</h2>
-                        <p>
-                            Partager les frais de vos trajets grâce au covoiturage. Entretenir le véhicule, des péages sur les trajets de longues distances, faire le plein d'essence, etc... est un gouffre financier. Le covoiturage est la solution pour faire des économies et tout le monde y gagne
-                        </p>
-                        <a href="#" class="btn">Débutez avec notre application</a>
-                    </div>
-                    <!--End of user ineract text -->
-                </div>
-                <div class="col-lg-7 col-sm-7">
-                    <!-- user interact image -->
-                    <div class="user-interact-image type2">
-                        <img src="assets/img/feature/user-interact2.png"  alt="">
-                    </div>
-                    <!-- End of user interact image -->
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- interact user -->
 
     <!-- KAMAROUDINE pour l'enregistrement des utilisateurs-->
     <!-- interact user -->
@@ -345,6 +318,34 @@
 
 
 
+     <!-- SYLVAIN pour Avantages  -->
+    <!-- interact user -->
+    <section class="pt-120 pb-120">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-sm-5">
+                    <!-- user ineract text -->
+                    <div class="user-interact-inner">
+                        <h2>Le covoiturage, c'est économique</h2>
+                        <p>
+                            Partager les frais de vos trajets grâce au covoiturage. Entretenir le véhicule, des péages sur les trajets de longues distances, faire le plein d'essence, etc... est un gouffre financier. Le covoiturage est la solution pour faire des économies et tout le monde y gagne
+                        </p>
+                        <a href="#" class="btn">Débutez avec notre application</a>
+                    </div>
+                    <!--End of user ineract text -->
+                </div>
+                <div class="col-lg-7 col-sm-7">
+                    <!-- user interact image -->
+                    <div class="user-interact-image type2">
+                        <img src="assets/img/feature/user-interact2.png"  alt="">
+                    </div>
+                    <!-- End of user interact image -->
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- interact user -->
+    
     <!-- SYLVAIN pour les avantages  -->
     <!-- interact user -->
     <section class="pt-120 pb-120">
