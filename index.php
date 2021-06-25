e3a2a11835d15ce562ba920a2c79608ab8facf9a
Black Legend <agbogawoluck@gmail.com>
2021-06-25 10:26:38 +0000

    modifications

--- index.php
+++ index.php
@@ -22,6 +22,9 @@
     <!--==== Font-Awesome css file ====-->
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
 
+    <!--==== BoxIcons css file ====-->
+    <link rel="stylesheet" href="assets/plugins/boxicons/css/boxicons.min.css">
+
     <!-- Owl Carusel css file -->
     <link rel="stylesheet" href="assets/plugins/owl-carousel/owl.carousel.min.css">
 
@@ -69,7 +72,7 @@
                             <div class="logo">
                                 <a href="index.html">
                                     <!-- <img src="assets/img/logo.png" data-rjs="2" alt="CoVoiturage"> -->
-                                    <h2>KONYIN</h2>
+                                    <h2>KOMYIN</h2>
                                 </a>
                             </div>
                             <!-- End of Logo -->
@@ -156,12 +159,12 @@
                 <div class="col-lg-5 col-sm-5">
                     <!-- user ineract text -->
                     <div class="user-interact-inner">
-                        <h2>C'est quoi <br> KONYIN ?</h2>
+                        <h2>C'est quoi <br> KOMYIN ?</h2>
                     </div>
                     <!--End of user ineract text -->
                 </div>
                 <div class="col-lg-7 col-sm-7">
-                    <p>KONYIN est une application qui vous permet de 
+                    <p>KOMYIN est une application qui vous permet de 
                         faciliter votre trajet quotidien où que vous soyez ou vous allez. <br>
                         L'application permettra à tous utilisateurs possédant des véhicules de publier ses trajets hebdomadaires 
                         et l'application se chargera de chercher des passagers sur votre trajet. <br>
@@ -1010,6 +1013,80 @@
     </section>
     <!-- end of blog artea -->
 
+    <!-- Contact -->
+    <section class="bg-2 pt-120 pb-120 contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-7 col-sm-7">
+                    <!-- user interact image -->
+                    <div class="user-interact-image">
+                        <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+                            <div class="form-row">
+                              <div class="col-md-6 form-group">
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Votre Nom" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                <div class="validate"></div>
+                              </div>
+                              <div class="col-md-6 form-group">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Votre Email" data-rule="email" data-msg="Please enter a valid email" />
+                                <div class="validate"></div>
+                              </div>
+                            </div>
+                            <div class="form-group">
+                              <input type="text" class="form-control" name="subject" id="subject" placeholder="Objet" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                              <div class="validate"></div>
+                            </div>
+                            <div class="form-group">
+                              <textarea class="form-control" name="message" rows="6" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                              <div class="validate"></div>
+                            </div>
+                            <div class="mb-3">
+                              <div class="loading">Loading</div>
+                              <div class="error-message"></div>
+                              <div class="sent-message">Your message has been sent. Thank you!</div>
+                            </div>
+                            <div class="text-center"><button type="submit">Envoyer</button></div>
+                          </form>
+                    </div>
+                    <!-- End of user interact image -->
+                </div>
+                <div class="col-lg-5 col-sm-5">
+                    <!-- user ineract text -->
+                    <div class="user-interact-inner">
+                        <h2>Nous contacter</h2>
+                        <div class="row">
+                            <div class="col-md-12">
+                              <div class="info-box">
+                                <i class="bx bx-share-alt"></i>
+                                <h3>Social Profiles</h3>
+                                <div class="social-links">
+                                  <a href="#" class="twitter"><i class="fa fa-facebook-f" aria-hidden="true"></i></a>
+                                  <a href="#" class="facebook"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                  <a href="#" class="instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                  
+                                </div>
+                              </div>
+                            </div>
+                            <div class="col-md-6">
+                              <div class="info-box mt-4">
+                                <i class="bx bx-envelope"></i>
+                                <h3>Email Me</h3>
+                                <p>contact@example.com</p>
+                              </div>
+                            </div>
+                            <div class="col-md-6">
+                              <div class="info-box mt-4">
+                                <i class="bx bx-phone-call"></i>
+                                <h3>Call Me</h3>
+                                <p>+1 5589 55488 55</p>
+                              </div>
+                            </div>
+                          </div>
+                    </div>
+                    <!--End of Contact -->
+                </div>
+            </div>
+        </div>
+    </section>
 
 
 
