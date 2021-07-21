730bf3654245ed31f828936dc4b5d6acfff334eb
letBreath <Letbreathtg@gmail.com>
2021-07-21 11:04:46 +0000

    modif du menu

--- index.php
+++ index.php
@@ -88,7 +88,7 @@
                                                 <li class=""><a href="#">Accueil</a></li>
                                                 <li class=""><a href="#a-propos">A propos </a>
                                                 <li><a href="#features">Services</a></li>
-                                                <li><a href="#app">App Screens</a></li>
+                                                <li><a href="#app">Télécharger l'application</a></li>
                                                     
 
                                                 </li>
